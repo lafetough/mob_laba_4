0760f4f99b8071df616595cf727e5ad3cfd412e5
lafetough <klad.vad@yandex.ru>
2025-09-17 12:04:32 +0300

    Добавление конвертеров для списков (List<String>)

--- app/src/main/java/com/example/laba4/AppDatabase.kt
+++ app/src/main/java/com/example/laba4/AppDatabase.kt
@@ -4,8 +4,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import android.content.Context
+import androidx.room.TypeConverters
 
 @Database(entities = [BreedData::class, BreedPreview::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun breedDataDao(): BreedDataDAO
     abstract fun breedPreviewDao(): BreedPreviewDAO
